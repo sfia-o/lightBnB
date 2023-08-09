0048af6ef20108964e71c963dc50d0201d1954d4
sfia-o <sofia.vl.oliveira@gmail.com>
2023-08-09 20:27:33 +0000

    Refactor functions to use database

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -18,14 +18,14 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function (email) {
-  let resolvedUser = null;
-  for (const userId in users) {
-    const user = users[userId];
-    if (user?.email.toLowerCase() === email?.toLowerCase()) {
-      resolvedUser = user;
-    }
-  }
-  return Promise.resolve(resolvedUser);
+  return pool
+  .query(`SELECT * FROM users WHERE email = $1;`, [email])
+  .then((result) => {
+    return result.rows[0];
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 };
 
 /**
@@ -34,7 +34,14 @@ const getUserWithEmail = function (email) {
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
+  return pool
+  .query(`SELECT * FROM users WHERE id = $1;`, [id])
+  .then((result) => {
+   return result.rows[0];
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 };
 
 /**
@@ -43,10 +50,16 @@ const getUserWithId = function (id) {
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  console.log('user', user);
+  return pool
+  .query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *;`, [user.name, user.email, user.password])
+  .then((result) => {
+    console.log('result', result);
+    return result.rows[0];
+  })
+  .catch((err) => {
+    console.log(err.message);
+  })
 };
 
 /// Reservations
@@ -71,9 +84,8 @@ const getAllReservations = function (guest_id, limit = 10) {
 
 const getAllProperties = (options, limit = 10) => {
   return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
+    .query(`SELECT * FROM properties LIMIT $1;`, [limit])
     .then((result) => {
-      console.log(result.rows);
       return result.rows;
     })
     .catch((err) => {
